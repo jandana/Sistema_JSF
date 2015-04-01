5b6348685c18945b116c9dfcaac9ef2b1e07b7a3
Javier <tenientedan123@gmail.com>
2015-04-01 20:08:27 -0300

    Se agrego una validacion com msg personalizado

--- src/beans/ClienteBean.java
+++ src/beans/ClienteBean.java
@@ -54,11 +54,7 @@ public class ClienteBean {
 	}
 
 	// Agrega un cliente
-<<<<<<< HEAD
 	public String agregarCliente() throws SQLException {
-=======
-	public String agregarCliente() {
->>>>>>> Sistema_JSF/master
 		daoCliente.agregarCliente(nuevoCliente.getRut(),
 				nuevoCliente.getNombre(), nuevoCliente.getApellidoP(),
 				nuevoCliente.getApellidoM(), nuevoCliente.getEmail());
@@ -67,11 +63,7 @@ public class ClienteBean {
 	}
 
 	// Elimina un cliente
-<<<<<<< HEAD
 	public String eliminarCliente() throws SQLException {
-=======
-	public String eliminarCliente() {
->>>>>>> Sistema_JSF/master
 		cliente = (Cliente) tablaTodosLosClientes.getRowData();
 		daoCliente.eliminaCliente(cliente.getRut());
 		return "eliminadoExitosamente";
